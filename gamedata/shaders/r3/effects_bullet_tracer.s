a4e5f513c87054973684da932b2664079157bdf1
Decane <arnalp@live.com>
2021-05-09 18:44:45 +0100

    SRP v1.1.4 WIP

--- gamedata/shaders/r3/effects_bullet_tracer.s
+++
@@ -1,7 +0,0 @@
-function normal(shader, t_base, t_second, t_detail)
-	shader	: begin	("stub_default", "stub_default")
-			: zb	(true, false)
-			: blend	(true, blend.one, blend.one)
-	shader 	: dx10texture("s_base", t_base)
-	shader 	: dx10sampler("smp_base")
-end
